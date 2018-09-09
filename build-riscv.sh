91d36f4e9e716e37eda55ba932d0dbb4c8d5d000
ronaldjmaas <32280203+ronaldjmaas@users.noreply.github.com>
2018-09-09 12:11:14 -0700

    Removed riscv64-unknown-elf- prefix.

--- build-riscv.sh
+++ build-riscv.sh
@@ -1,8 +1,8 @@
-riscv64-unknown-elf-g++ -S -Os -fno-inline -fno-builtin *.cpp
-riscv64-unknown-elf-as -ahln CryptUtils.s >bin/CryptUtils-riscv.txt
-riscv64-unknown-elf-as -ahln RijndaelEncrypter.s >bin/RijndaelEncrypter-riscv.txt
-riscv64-unknown-elf-as -ahln RijndaelTest.s >bin/RijndaelTest-riscv.txt
+g++ -S -Os -fno-inline -fno-builtin *.cpp
+as -ahln CryptUtils.s >bin/CryptUtils-riscv.txt
+as -ahln RijndaelEncrypter.s >bin/RijndaelEncrypter-riscv.txt
+as -ahln RijndaelTest.s >bin/RijndaelTest-riscv.txt
 
-riscv64-unknown-elf-g++ -Os -fno-inline -fno-builtin -o bin/Rijndael-riscv *.cpp
-riscv64-unknown-elf-objdump -d -M numeric bin/Rijndael-riscv >bin/Rijndael-riscv.txt
+g++ -Os -fno-inline -fno-builtin -o bin/Rijndael-riscv *.cpp
+objdump -d -M numeric bin/Rijndael-riscv >bin/Rijndael-riscv.txt
 rm a.out
